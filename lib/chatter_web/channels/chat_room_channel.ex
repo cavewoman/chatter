88801e36402f28049b5cd0d104c81a0cd102c5f8
cavewoman <anna.chan365@gmail.com>
2018-10-20 12:59:40 -0400

    [Anna] Working chat app with persistence

--- lib/chatter_web/channels/chat_room_channel.ex
+++ lib/chatter_web/channels/chat_room_channel.ex
@@ -3,10 +3,21 @@ defmodule ChatterWeb.ChatRoomChannel do
 
   def join("chat_room:lobby", payload, socket) do
     if authorized?(payload) do
+      send(self(), :after_join)
       {:ok, socket}
     else
       {:error, %{reason: "unauthorized"}}
     end
+  end
+
+  def handle_info(:after_join, socket) do
+    IO.puts "<><><>CALLING HANDLE INFO<><><><>"
+    Chatter.Message.recent_messages()
+    |> Enum.each(fn msg -> push(socket, "new_message", %{
+      name: msg.name,
+      message: msg.message
+    }) end)
+    {:noreply, socket}
   end
 
   # Channels can be used in a request/response fashion
@@ -17,9 +28,15 @@ defmodule ChatterWeb.ChatRoomChannel do
 
   # It is also common to receive messages from the client and
   # broadcast to everyone in the current topic (chat_room:lobby).
-  def handle_in("shout", payload, socket) do
-    broadcast socket, "shout", payload
+  def handle_in("new_message", payload, socket) do
+    spawn(fn -> save_message(payload) end)
+    broadcast! socket, "new_message", payload
     {:noreply, socket}
+  end
+
+  defp save_message(message) do
+    Chatter.Message.changeset(%Chatter.Message{}, message)
+      |> Chatter.Repo.insert
   end
 
   # Add authorization logic here as required.
